727332adc21fa71affcbc4b9a639eb0ec17336ca
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-05-13 19:32:31 -0500

    2.3 create a new branch

---
+++ app.js
@@ -0,0 +1,42 @@
+//console.log('Hello Node!');
+//console.log(document);
+// var message = "Hello Node!";
+
+// var sum = 5 + 3;
+
+// console.log(message);
+// console.log(sum);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+//Notice the lack of parentheses around the 'profileDataArr' parameter?
+
+//     for (let i =0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+// };
+//printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+// const printProfileData = profileDataArr => {
+
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('================');
+
+//     // Is the same as this...
+//     // profileDataArr.forEach((profileItem) => {
+//     //     console.log(profileItem)
+//     //});
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
