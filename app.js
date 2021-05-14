a6f89b405549e259579ba37fb958fb9e4b1c5bec
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-05-13 20:32:56 -0500

    2.4 create an HTML template

--- app.js
+++ app.js
@@ -20,7 +20,7 @@
 //     }
 // };
 //printProfileData(profileDataArgs);
-const profileDataArgs = process.argv.slice(2);
+// const profileDataArgs = process.argv.slice(2);
 
 // const printProfileData = profileDataArr => {
 
@@ -39,4 +39,29 @@ const profileDataArgs = process.argv.slice(2);
 // };
 
 // printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
+const generatePage = (userName, githubName) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=devive-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+};
+
+console.log(name, github);
+console.log(generatePage(name, github));
