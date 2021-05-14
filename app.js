87705a430ed8aa223824626937af816e3cbc964f
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-05-13 19:13:08 -0500

    1.1-1.7

---
+++ app.js
@@ -0,0 +1,37 @@
+//console.log('Hello Node!');
+//console.log(document);
+// var message = "Hello Node!";
+
+// var sum = 5 + 3;
+
+// console.log(message);
+// console.log(sum);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+//Notice the lack of parentheses around the 'profileDataArr' parameter?
+const printProfileData = profileDataArr => {
+//     for (let i =0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+// };
+//printProfileData(profileDataArgs);
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('================');
+
+    // Is the same as this...
+    // profileDataArr.forEach((profileItem) => {
+    //     console.log(profileItem)
+    //});
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+    
+};
+
