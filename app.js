41e7259211a7c01f4abdc090b1d12967d18182b4
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-01 10:27:46 -0500

    1.4 Get started with node.js

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 //console.log('Hello Node!');
-//console.log(document);
-// var message = "Hello Node!";
+console.log(document);
+// // var message = "Hello Node!";
 
-// var sum = 5 + 3;
+// // var sum = 5 + 3;
 
-// console.log(message);
-// console.log(sum);
+// // console.log(message);
+// // console.log(sum);
 
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
+// // var commandLineArgs = process.argv;
+// // console.log(commandLineArgs);
 
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+// // const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // console.log(profileDataArgs);
 
-//Notice the lack of parentheses around the 'profileDataArr' parameter?
+// //Notice the lack of parentheses around the 'profileDataArr' parameter?
 
-//     for (let i =0; i < profileDataArr.length; i++) {
+// //     for (let i =0; i < profileDataArr.length; i++) {
+// //         console.log(profileDataArr[i]);
+// //     }
+// // };
+// //printProfileData(profileDataArgs);
+// const profileDataArgs = process.argv.slice(2);
+
+// const printProfileData = profileDataArr => {
+
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
 //         console.log(profileDataArr[i]);
 //     }
+
+//     console.log('================');
+
+//     // Is the same as this...
+//     // profileDataArr.forEach((profileItem) => {
+//     //     console.log(profileItem)
+//     //});
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
 // };
-//printProfileData(profileDataArgs);
-const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('================');
-
-    // Is the same as this...
-    // profileDataArr.forEach((profileItem) => {
-    //     console.log(profileItem)
-    //});
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
 
