054594bd157893930dd19106bf87b64b107387da
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-01 11:14:20 -0500

    1.5 Capture User Input

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 //console.log('Hello Node!');
-console.log(document);
-// // var message = "Hello Node!";
+//console.log(document);
 
-// // var sum = 5 + 3;
+// var message = "Hello Node!";
+// var sum = 5 + 3;
 
-// // console.log(message);
-// // console.log(sum);
+// console.log(message);
+// console.log(sum);
 
-// // var commandLineArgs = process.argv;
-// // console.log(commandLineArgs);
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
 
-// // const profileDataArgs = process.argv.slice(2, process.argv.length);
-// // console.log(profileDataArgs);
+//The slice method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) 
+    //where start and end represent the index of items in that array. The originall array will not be modified.
+// var profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+//const keyword (constant) is a feature in JavaScript that allows us to create variables that can't be reassigned a value
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// const message = 'Hello Node!';
+// message = 'Goodbye Node!';
+
+// const sum = 5 + 3;
+// sum += 1;
+
+// console.log(message);
+// console.log(sum);
+
+const animalArray = ['dog', 'cat', 'pig'];
+animalArray.push('cow');
+
+const personObj = {
+    name:'Lernantino',
+    age: '99'
+};
+personObj.age = 100;
+personObj.occupation = 'Developer';
+
+console.log(animalArray);
+console.log(personObj);
 
 // //Notice the lack of parentheses around the 'profileDataArr' parameter?
 
