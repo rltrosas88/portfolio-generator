69db28df1e0f72b3fa01ec20c6de9bee01e853b8
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-04 11:14:08 -0500

    3.5 part 1 of create the .gitignore file complete

--- app.js
+++ app.js
@@ -1,37 +1,50 @@
-//The require statement is a built-in function that's globally available in Node.js that allows the app.js file to access the fs module's functions through the fs assignment
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+//console.log(inquirer);
 
-//console.log('Hello Node!');
-//console.log(document);
+// //The require statement is a built-in function that's globally available in Node.js that allows the app.js file to access the fs module's functions through the fs assignment
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-// var message = "Hello Node!";
-// var sum = 5 + 3;
+// //console.log('Hello Node!');
+// //console.log(document);
 
-// console.log(message);
-// console.log(sum);
+// // var message = "Hello Node!";
+// // var sum = 5 + 3;
 
-const profileDataArgs = process.argv.slice(2);
+// // console.log(message);
+// // console.log(sum);
 
-console.log(profileDataArgs);
+// // const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
+// // console.log(profileDataArgs);
 
-console.log(name, github);
+// // const [name, github] = profileDataArgs;
 
-const pageHTML = generatePage(name, github);
+// // console.log(name, github);
 
-// fs.writeFile(file, data[,options], callback)
-    //fs.writeFile has 3 arguements
-        //1) the file name that will be created, or the output file
-        //2) the data that's being written: the HTML string template
-        //3) the callback function that will handle any errors as well as the success message
-    //When an arrow function has one argument, parentheses are optional
-    //When an arrow function has no arguments -or more than one- parentheses are necessary
-fs.writeFile('./index.html', pageHTML, err => {
-    //a conditional statement that checks for the err being returned by the callback function
-    //if err exists, an error message is displayed
-    if (err) throw new err;
+// const pageHTML = generatePage(name, github);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// // fs.writeFile(file, data[,options], callback)
+//     //fs.writeFile has 3 arguements
+//         //1) the file name that will be created, or the output file
+//         //2) the data that's being written: the HTML string template
+//         //3) the callback function that will handle any errors as well as the success message
+//     //When an arrow function has one argument, parentheses are optional
+//     //When an arrow function has no arguments -or more than one- parentheses are necessary
+// fs.writeFile('./index.html', pageHTML, err => {
+//     //a conditional statement that checks for the err being returned by the callback function
+//     //if err exists, an error message is displayed
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
